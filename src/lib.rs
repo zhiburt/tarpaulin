948460c804c25b1d5c1c81b25bd73c4ec430c39c
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-19 19:17:59 -0500

    Only try to limit affinity on linux

--- src/lib.rs
+++ src/lib.rs
@@ -117,8 +117,10 @@ pub fn get_test_coverage(
     if !test.exists() {
         return Ok(None);
     }
-    if let Err(e) = limit_affinity() {
-        warn!("Failed to set processor affinity {}", e);
+    #[cfg(target_os = "linux")] {
+        if let Err(e) = limit_affinity() {
+            warn!("Failed to set processor affinity {}", e);
+        }
     }
     match fork() {
         Ok(ForkResult::Parent { child }) => match collect_coverage(test, child, analysis, config) {

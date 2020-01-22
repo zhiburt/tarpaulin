5616218e96dc222cf9f07819d97c6ba3511357c3
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-22 17:55:25 -0500

    macOS - Generate mach interface at compile time

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+use std::path::{PathBuf};
+use std::fs;
+
+const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
+const MACH_EXC_SERVER: &'static str = "mach_excServer";
+fn main() {
+    let mut mach_src_path = PathBuf::from("mach_interface/");
+    if !mach_src_path.exists() {
+        fs::create_dir(&mach_src_path).unwrap();
+
+        Command::new("mig")
+            .arg(MACH_DEFS)
+            .current_dir(&mach_src_path)
+            .output()
+            .unwrap();
+    }
+    mach_src_path.push(MACH_EXC_SERVER);
+    mach_src_path.set_extension("c");
+    cc::Build::new()
+        .file(mach_src_path)
+        .compile(MACH_EXC_SERVER);
+}

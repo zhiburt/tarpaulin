36783f3d2dee62eee911b83f5a0ec5a69127b0e3
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-22 17:55:25 -0500

    macOS - Generate mach interface at compile time

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 use rustc_version::{version, version_meta, Channel};
+use std::process::Command;
+use std::path::{PathBuf};
+use std::fs;
+
+const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
+const MACH_EXC_SERVER: &'static str = "mach_excServer";
 
 fn main() {
     assert!(version().expect("Couldn't get compiler version").major >= 1);
@@ -9,4 +15,20 @@ fn main() {
     if channel == Channel::Nightly {
         println!("cargo:rustc-cfg=nightly");
     }
+
+    let mut mach_src_path = PathBuf::from("mach_interface/");
+    if !mach_src_path.exists() {
+        fs::create_dir(&mach_src_path).unwrap();
+
+        Command::new("mig")
+            .arg(MACH_DEFS)
+            .current_dir(&mach_src_path)
+            .output()
+            .unwrap();
+    }
+    mach_src_path.push(MACH_EXC_SERVER);
+    mach_src_path.set_extension("c");
+    cc::Build::new()
+        .file(mach_src_path)
+        .compile(MACH_EXC_SERVER);
 }

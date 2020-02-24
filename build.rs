fc2a90a6cd6eb18219688307358ea8ea9b2c2c28
xd009642 <danielmckenna93@gmail.com>
2020-02-24 21:53:44 +0000

    Update build.rs to work on linux again

--- build.rs
+++ build.rs
@@ -1,20 +1,15 @@
 use rustc_version::{version, version_meta, Channel};
+#[cfg(target_os = "macos")]
 use std::fs;
+#[cfg(target_os = "macos")]
 use std::path::PathBuf;
+#[cfg(target_os = "macos")]
 use std::process::Command;
 
-const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
-const MACH_EXC_SERVER: &'static str = "mach_excServer";
-
-fn main() {
-    assert!(version().expect("Couldn't get compiler version").major >= 1);
-
-    let channel = version_meta()
-        .expect("Couldn't get compiler metadata")
-        .channel;
-    if channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=nightly");
-    }
+#[cfg(target_os = "macos")]
+fn setup_macos() {
+    const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
+    const MACH_EXC_SERVER: &'static str = "mach_excServer";
 
     let mut mach_src_path = PathBuf::from("mach_interface/");
     if !mach_src_path.exists() {
@@ -31,4 +26,17 @@ fn main() {
     cc::Build::new()
         .file(mach_src_path)
         .compile(MACH_EXC_SERVER);
+}
+
+fn main() {
+    assert!(version().expect("Couldn't get compiler version").major >= 1);
+
+    let channel = version_meta()
+        .expect("Couldn't get compiler metadata")
+        .channel;
+    if channel == Channel::Nightly {
+        println!("cargo:rustc-cfg=nightly");
+    }
+    #[cfg(target_os = "macos")]
+    setup_macos();
 }

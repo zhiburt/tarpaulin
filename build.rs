06f4276a3d573138baeb897ee8775a1f31970532
xd009642 <danielmckenna93@gmail.com>
2020-02-24 21:53:44 +0000

    Update build.rs to work on linux again

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
+#[cfg(target_os = "macos")]
 use std::fs;
+#[cfg(target_os = "macos")]
 use std::path::PathBuf;
+#[cfg(target_os = "macos")]
 use std::process::Command;
 
-const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
-const MACH_EXC_SERVER: &'static str = "mach_excServer";
-fn main() {
+#[cfg(target_os = "macos")]
+fn setup_macos() {
+    const MACH_DEFS: &'static str = "/usr/include/mach/mach_exc.defs";
+    const MACH_EXC_SERVER: &'static str = "mach_excServer";
+
     let mut mach_src_path = PathBuf::from("mach_interface/");
     if !mach_src_path.exists() {
         fs::create_dir(&mach_src_path).unwrap();
@@ -20,4 +25,9 @@ fn main() {
     cc::Build::new()
         .file(mach_src_path)
         .compile(MACH_EXC_SERVER);
+}
+
+fn main() {
+    #[cfg(target_os = "macos")]
+    setup_macos();
 }
